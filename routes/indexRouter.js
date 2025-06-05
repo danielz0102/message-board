7c3c5f841bc3523de07e87494222c5f22ba344c8
danielz0102 <danyzglez@hotmail.com>
2025-06-04 19:08:36 -0600

    feat: render messages on index page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 export const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { messages });
 });
