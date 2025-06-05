cd860a1e4e74793e9838f6c1b80b516014ecbd28
danielz0102 <danyzglez@hotmail.com>
2025-06-04 19:01:00 -0600

    feat: set up project

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { indexRouter } from "./routes/indexRouter.js";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.set("views", process.cwd() + "/views");
+app.set("view engine", "ejs");
+
+app.use(express.static(process.cwd() + "/public"));
+app.use("/", indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
