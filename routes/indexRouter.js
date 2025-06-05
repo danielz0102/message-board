33b5bcdcf75ccb27b64b1fc2902656be145a90e7
danielz0102 <danyzglez@hotmail.com>
2025-06-04 19:37:26 -0600

    feat: add message detail page
    To achieve this, an id attribute is added to the messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 
 const messages = [
   {
+    id: crypto.randomUUID(),
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: crypto.randomUUID(),
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
@@ -21,8 +23,18 @@ indexRouter.get("/", (req, res) => {
 indexRouter.get("/new", (req, res) => {
   res.render("form");
 });
+indexRouter.get("/detail/:id", (req, res) => {
+  const { id } = req.params;
+  const msg = messages.find((message) => message.id === id);
+
+  if (!msg) {
+    return res.status(404).send("Message not found");
+  }
+
+  res.render("detail", { msg });
+});
 indexRouter.post("/new", (req, res) => {
   const { user, text } = req.body;
-  messages.push({ user, text, added: new Date() });
+  messages.push({ id: crypto.randomUUID(), user, text, added: new Date() });
   res.redirect("/");
 });
