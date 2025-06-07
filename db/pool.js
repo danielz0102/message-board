ffe7c468bb69f21ed3b3b60f180e1e8a6e6f4300
danielz0102 <danyzglez@hotmail.com>
2025-06-06 18:16:49 -0600

    refactor(pool.js): connect with URI instead of object

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,5 @@ import { Pool } from "pg";
 import "dotenv/config";
 
 export default new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_CONNECTION_URI,
 });
